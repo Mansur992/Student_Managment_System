e7169311bc378c3ca4935ae3a4d91745abc05150
Mansur020 <mamedovmansur@gmail.com>
2023-09-02 15:02:53 +0400

    Test
    Test

---
+++ src/main/java/com/javaguides/sms/StudentManagmentSystemApplication.java
@@ -0,0 +1,22 @@
+package com.javaguides.sms;
+
+import com.javaguides.sms.entity.Student;
+import com.javaguides.sms.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagmentSystemApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagmentSystemApplication.class, args);
+	}
+	@Autowired
+	private StudentRepository studentRepository;
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
